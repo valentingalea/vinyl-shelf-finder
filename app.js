58b913ce373fae5e18b3c0a282beae8774163da8
valentingalea <valentingalea@gmail.com>
2017-08-12 15:30:09 +0100

    express.js REST server

--- app.js
+++ app.js
@@ -1,15 +1,39 @@
+const stringifyObject = require('stringify-object');
+
+//
+// Discogs API
+//
 var Discogs = require('disconnect').Client;
 var UserAgent = 'vinyl-shelf-finder/1.0';
 var User = 'valentingalea';
 
-//var db = new Discogs(UserAgent).database();
+// var db = new Discogs(UserAgent).database();
 // db.getRelease(176126, function(err, data){
-// 	console.log(data);
+//     console.log(data);
 // });
 
 var col = new Discogs(UserAgent).user().collection();
-col.getReleases(User, 0, {}, function(err, data){
-    var count = data.releases.length;
-    var index = Math.round(Math.random() * count);
-	console.log(data.releases[index]);
+
+
+//
+// Express REST server
+//
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send(UserAgent);
+});
+
+app.get('/random', function (req, res) {
+    col.getReleases(User, 0, {}, function(err, data){
+        var count = data.releases.length;
+        var index = Math.round(Math.random() * count);
+        var msg = data.releases[index];
+        res.send("<pre>" + stringifyObject(msg) + "</pre>");
+    });
+});
+
+app.listen(8080, function () {
+    console.log('Listening...');
 });
