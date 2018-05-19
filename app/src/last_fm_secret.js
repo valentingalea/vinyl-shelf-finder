36c364f9669eebf56d1b0bf5ad1f6648e80782b5
Valentin Galea <valentingalea@gmail.com>
2018-05-19 20:21:04 +0000

    add last.fm secret file as tutorial

---
+++ app/src/last_fm_secret.js
@@ -0,0 +1,6 @@
+const Secrets = module.exports = { 
+    apiKey: "<get it from Last.fm Dev settings>", 
+    apiSecret: "<get it from Last.fm Dev settings>",
+    username: "<your last.fm account>",
+    password: "<your last.fm password>"
+};
