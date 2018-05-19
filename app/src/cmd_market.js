3ec85f55073c7f4fe7df94501477e00210e6d7e8
Valentin Galea <valentingalea@gmail.com>
2018-05-20 00:11:32 +0100

    working price checker

--- app/src/cmd_market.js
+++ app/src/cmd_market.js
@@ -6,22 +6,35 @@ const cmd_search = require('./cmd_search.js');
 var market = module.exports = {};
 
 market.price_of_box = function(query, res) {
-    let total = [];
     let box = cmd_search.search(query);
-    marketplace_iter(box, total, res);
+    marketplace_iter(box, res);
 };
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function marketplace_iter(box, total, res) {
+async function marketplace_iter(box, res) {
+    let output = [];
+    let total = 0.;
+    
     for (let i = 0; i < box.length; i++) {
-        let price = undefined;
-        price = await DG.market.getPriceSuggestions(box[i].id)
+        let price_info = undefined;
+        price_info = await DG.market.getPriceSuggestions(box[i].id)
             .catch((err) => { console.log(err); });
-        await sleep(25);
-        total.push(price);
+
+        await sleep(1000./60.);
+
+        let price = price_info["Good Plus (G+)"].value;
+        total += price;
+        output.push({
+            'id': ("https://www.discogs.com/release/" + box[i].id),
+            'price': price
+        });
     }
-    res.send(total);
+
+    res.send({
+        'total': total,
+        'breakdown': output
+    });
 };
